e9f31de0e7a808f41f4938314f2dcfdb6e7777ef
Ayush-dixit-15 <105589099+Ayush-dixit-15@users.noreply.github.com>
2022-06-19 02:19:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.nav-list');
+rightnav = document.querySelector('.rightnav');
+burger.addEventListener('click', ()=>{
+   rightnav.classList.toggle('v-class');
+   navlist.classList.toggle('v-class');
+   navbar.classList.toggle('h-nav');
+//    all this means ki jab bhi kbhee ispe click kro to ye waali class gaayb ho jaaye aur baaki rhee bchee classes se jo bhi hmne css lgaai hai wo show hone lg jaaye
+  
+})
