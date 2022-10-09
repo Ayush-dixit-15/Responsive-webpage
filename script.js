d6663832d30e801a1f658ae4de8f2410810e25fc
Piyush <piyushyadav11102002@gmail.com>
2022-10-09 16:35:52 +0530

    fix:fixed the navbar for phone view

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ burger.addEventListener('click', ()=>{
    rightnav.classList.toggle('v-class');
    navlist.classList.toggle('v-class');
    navbar.classList.toggle('h-nav');
+   navbar.classList.toggle('show');
 //    all this means ki jab bhi kbhee ispe click kro to ye waali class gaayb ho jaaye aur baaki rhee bchee classes se jo bhi hmne css lgaai hai wo show hone lg jaaye
   
 })
